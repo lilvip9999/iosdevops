b5d5af087bf76deac61ea031f6f31b01c96767c4
Vuong Vu <vuong.vv@homecredit.vn>
2020-12-31 12:28:22 +0700

    - init project

---
+++ devopsdemo/devopsdemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  devopsdemo
+//
+//  Created by Vuong Vu Van (VN) on 31/12/2020.
+//  Copyright © 2020 Vuong Vu Van (VN). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
